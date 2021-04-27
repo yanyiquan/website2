649ded275279a2a677498608a200fd3e7082bcd6
DucQuan12 <quanbn1336@gmail.com>
2021-04-27 20:21:11 +0700

    scrip run app

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo -e "Start App"
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
